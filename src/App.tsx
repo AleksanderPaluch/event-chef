82b8586403abb039dbb38daa19400c18ae040102
Aleksander Paluch <aleksander.paluch@wp.pl>
2026-01-10 21:47:01 +0100

    markup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { Content } from './components/Content/Content'
-import { Nav } from './components/Nav/Nav'
-import { Testimonials } from './components/Testimonials/Testimonials'
+import { Content } from "./components/Content/Content";
+import { FormSection } from "./components/FormSection/FormSection";
+import { Links } from "./components/Links/Links";
+import { Nav } from "./components/Nav/Nav";
+import { Stats } from "./components/Stats/Stats";
+import { Table } from "./components/Table/Table";
+import { Testimonials } from "./components/Testimonials/Testimonials";
 
 function App() {
-
-
   return (
     <>
-    <Nav />
-    <Content />
-    <Testimonials />
+      <Nav />
+      <Content />
+      <Stats />
+      <Table />
+      <Testimonials />
+      <FormSection />
+      <Links />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
