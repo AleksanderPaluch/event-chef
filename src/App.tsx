067c1dbc003c7dbc67025128314005b01c252ecc
Aleksander Paluch <aleksander.paluch@wp.pl>
2026-01-16 20:21:56 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Live } from "./components/Live/Live";
 import { Mastercalss } from "./components/Masterclass/Mastercalss";
-import { Nav } from "./components/Nav/Nav";
+
 import { Omakase } from "./components/Omakase/Omakase";
 
 function App() {
