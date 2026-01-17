96804806530aa51be8a1ebe14392ca3578ee4222
Aleksander Paluch <aleksander.paluch@wp.pl>
2026-01-17 13:51:56 +0100

    nav +

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Live } from "./components/Live/Live";
 import { Mastercalss } from "./components/Masterclass/Mastercalss";
+import { Nav } from "./components/Nav/Nav";
 
 import { Omakase } from "./components/Omakase/Omakase";
 
 function App() {
   return (
     <>
-      {/* <Nav /> */}
+      <Nav />
       <Live />
       <Mastercalss />
       <Omakase />
