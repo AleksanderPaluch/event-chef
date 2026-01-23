a947e9765cd7094c461e3e25e51284d56f9643eb
Aleksander Paluch <aleksander.paluch@wp.pl>
2026-01-23 13:49:53 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Mastercalss } from "./components/Masterclass/Mastercalss";
 import { Nav } from "./components/Nav/Nav";
 import { Omakase } from "./components/Omakase/Omakase";
 import { About } from "./components/About/About";
-import { Offer } from "./components/Offer/Offer";
+
 
 // import { Offer } from "./components/Offer/Offer";
 
