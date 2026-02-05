7c49e52600a5e2030a652549f95045f92e2e90b5
Aleksander Paluch <aleksander.paluch@wp.pl>
2026-02-05 18:53:38 +0100

    tooltip test

--- src/components/Modal/Cards/TitleCard.tsx
+++ src/components/Modal/Cards/TitleCard.tsx
@@ -13,34 +13,32 @@ export const TitleCard: React.FC<CardProps> = ({
 }) => {
   return (
     <div className="relative px-4 py-2 bg-neutral-950 text-zinc-300 md:p-9 lg:h-80">
-      {/* Іконка — peer (ВАЖЛИВО: ПЕРШОЮ) */}
       <span className="absolute z-30 text-2xl cursor-pointer peer right-2 top-2">
         {icon}
       </span>
 
-      {/* Заголовок */}
-      <h2 className="relative z-10 text-3xl uppercase">
+      <h2 className="relative text-3xl uppercase z-999">
         Poznaj <br /> {textTitle}
       </h2>
 
-      {/* Tooltip ПІСЛЯ peer */}
       <p
         className="
-          absolute left-0 right-0
+          absolute left-0 right-4
           top-0
           lg:top-[calc(1.5rem+5rem)]
-          z-20
-          mt-2
+          lg:h-40
+          z-1
+          mt-0
           rounded-md bg-neutral-950
           px-2
           lg:px-7 py-2
-        h-20
+        h-full flex justify-center flex-col
           opacity-0 translate-y-2
           pointer-events-none
           transition-all duration-300
           peer-hover:opacity-100
           peer-hover:translate-y-0
-          text-xs italic fon text-zinc-500 lg:text-sm
+          text-md italic fon text-zinc-500 lg:text-md
         "
       >
         {modalDescription}
